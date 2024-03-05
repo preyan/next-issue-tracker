c3afe1f2cde9240c7d6540a7a6dace5f62ac5044
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-03-05 16:15:33 +0530

    style navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
-
+import classNames from "classnames";
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  console.log("currentPath", currentPath);
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -17,7 +24,12 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800": true,
+                "transition-colors": true,
+              })}
               href={link.href}
             >
               {link.label}
