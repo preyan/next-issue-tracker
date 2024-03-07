315a8746cc8840e5bc3a2d9cddd0dd7fd4c1eed1
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-03-07 12:04:20 +0530

    refactor: Add EditIssueButton and IssueDetails components for SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import ReactMarkdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 import { notFound } from "next/navigation";
 import prisma from "@/prisma/client";
 
@@ -9,7 +9,7 @@ interface Props {
   params: { id: string };
 }
 
-const IssueDetails = async ({ params }: Props) => {
+const IssueDetailsPage = async ({ params }: Props) => {
   if (typeof params.id !== "string") {
     notFound();
   }
@@ -25,17 +25,15 @@ const IssueDetails = async ({ params }: Props) => {
   }
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose mt-3">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.id} />
+      </Box>
+    </Grid>
   );
 };
 
-export default IssueDetails;
+export default IssueDetailsPage;
