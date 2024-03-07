5a1d96666c205a17a09f4461a9fedcb82a37f393
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-03-07 12:38:26 +0530

    Refactor issue route handlers

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { issueSchema } from "@/app/validationSchemas";
+import primsa from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validated = issueSchema.safeParse(body);
+  if (!validated.success) {
+    return NextResponse.json(validated.error.format(), { status: 400 });
+  }
+
+  const issue = await primsa.issue.findUnique({
+    where: { id: params.id },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  const updatedIssue = await primsa.issue.update({
+    where: { id: params.id },
+    data: {
+      title: validated.data.title,
+      description: validated.data.description,
+    },
+  });
+
+  if (!updatedIssue) {
+    return NextResponse.json({ error: "Issue update failed" }, { status: 500 });
+  }
+
+  return NextResponse.json(updatedIssue);
+}
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { createIssueSchema } from "../../validationSchemas";
+import { issueSchema } from "../../validationSchemas";
 import primsa from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validated = createIssueSchema.safeParse(body);
+  const validated = issueSchema.safeParse(body);
   if (!validated.success) {
     return NextResponse.json(validated.error.format(), { status: 400 }); //Bad request
   }
