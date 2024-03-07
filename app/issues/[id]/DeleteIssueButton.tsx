8aa5e3effd86227d03bc56d402fd448640fdcc2a
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-03-07 15:33:55 +0530

    Add AlertDialog for delete issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,14 +1,39 @@
-import { AiFillDelete } from "react-icons/ai";
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+
 import Link from "next/link";
 import React from "react";
 
-const EditIssueButton = ({ issueId }: { issueId: string }) => {
+const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete the issue? This is a permanent action
+          and cannot be undone.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+              {/* <Link href={`/issues/${issueId}/delete`}>Delete</Link> */}
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
-export default EditIssueButton;
+export default DeleteIssueButton;
